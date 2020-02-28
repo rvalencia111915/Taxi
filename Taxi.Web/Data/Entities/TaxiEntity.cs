37d3de55d4d60c9b333874b736e46229f3042fa1
rvalencia111915 <raesvalencia@gmail.com>
2020-02-28 08:22:59 -0500

    Preparation of backend and add trip and trip details

---
+++ Taxi.Web/Data/Entities/TaxiEntity.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Taxi.Web.Data.Entities
+{
+    public class TaxiEntity
+    {
+        public int ID { get; set; }
+
+        [StringLength(6, MinimumLength = 6, ErrorMessage = "The {0} field must have {1} characters.")]
+        [RegularExpression(@"^([A-Za-z]{3}\d{3})$", ErrorMessage = "The field {0} must starts whit three characters and ends whit three numbers.")]
+        [Required(ErrorMessage = "The field {0} is mandatory.")]
+
+        public string Plaque { get; set; }
+
+
+    }
+}
